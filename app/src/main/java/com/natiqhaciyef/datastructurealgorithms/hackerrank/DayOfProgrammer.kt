ddfde14ba92d8f5ccf27a89d6f6cb4ce9c3f2b49
Natiq <natiq00h2272@gmail.com>
2022-10-13 21:10:34 +0400

    Day of Programmer task

--- app/src/main/java/com/natiqhaciyef/datastructurealgorithms/hackerrank/DayOfProgrammer.kt
+++ app/src/main/java/com/natiqhaciyef/datastructurealgorithms/hackerrank/DayOfProgrammer.kt
@@ -1,5 +1,21 @@
 package com.natiqhaciyef.datastructurealgorithms.hackerrank
 
-fun main() {
+import java.time.LocalDateTime
+import java.time.Month
+
+fun dayOfProgrammer(year: Int): String {
+    return if (year > 1918) {
+        val calendar1 = LocalDateTime.of(year, Month.SEPTEMBER, 12, 0, 0)
+        val calendar2 = LocalDateTime.of(year, Month.SEPTEMBER, 13, 0, 0)
+        if (calendar1.dayOfYear == 256)
+            "${calendar1.dayOfMonth}.09.${calendar1.year}"
+        else if (calendar2.dayOfYear == 256)
+            "${calendar2.dayOfMonth}.09.${calendar2.year}"
+        else
+            "Error"
+    }else if (year == 1918)
+        "26.09.1918"
+    else
+        if (year % 4 != 0) "13.09.$year" else "12.09.$year"
 
 }
